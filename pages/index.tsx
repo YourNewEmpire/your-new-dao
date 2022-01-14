79624917ddeb1a2dd61bf189c4eb67ec2b064c9a
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-14 00:41:14 +0000

    index - added title and subtitle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
-    <div className='min-h-screen bg-slate-900' >
-       <h1 className='text-2xl'>home</h1>
+    <div className="min-h-screen">
+      <div className="flex flex-col items-center justify-center text-center ">
+        <h1 className="text-6xl text-center font-semibold">Your New Dao</h1>
+        <p>A Marketplace of DAOs</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
