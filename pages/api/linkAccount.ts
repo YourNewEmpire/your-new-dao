a242e3539aeca780f53e464c54b69a96e52d0ad9
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-20 18:39:19 +0000

    linkAccount api added - link user to DAO

---
+++ pages/api/linkAccount.ts
@@ -0,0 +1,109 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Moralis from "moralis/node";
+import contractInterface from "../../Hardhat/artifacts/contracts/DAO.sol/DAO.json";
+import { ethers } from "ethers";
+import { exit } from "process";
+const serverUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
+const appId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
+const masterKey = process.env.MORALIS_MASTER_KEY;
+const linkAccount = async (req: NextApiRequest, res: NextApiResponse) => {
+  const NODE_URL_MATIC = `https://rpc-mainnet.maticvigil.com/v1/${process.env.MATIC_NODE}`;
+  const NODE_URL_MUMBAI = `https://rpc-mumbai.maticvigil.com/v1/${process.env.MATIC_NODE}`;
+
+  const maticProvider = new ethers.providers.JsonRpcProvider(NODE_URL_MATIC);
+  const mumbaiProvider = new ethers.providers.JsonRpcProvider(NODE_URL_MUMBAI);
+  const { ownerAddress, daoAddress, chain, version } = req.body;
+
+  const readableAbi = ["function isOwner(address memory) view returns(bool)"];
+  const abi = [
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      name: "isOwner",
+      outputs: [
+        {
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  const contract = new ethers.Contract(
+    daoAddress,
+    abi,
+    chain === "matic" ? maticProvider : mumbaiProvider
+  );
+  Moralis.start({ serverUrl, appId, masterKey });
+  if (!chain) {
+    res
+      .status(403)
+      .send(
+        "no chain was specified. need a chain to know where to query with address"
+      );
+  }
+
+  const AlreadyLinked = Moralis.Object.extend("LinkedDaos");
+  const query = new Moralis.Query(AlreadyLinked);
+  query.equalTo("ownerAddress", ownerAddress);
+  query.equalTo("daoAddress", daoAddress);
+  return new Promise<void>((resolve, reject) => {
+    query
+      .find()
+      .then((result: any) => {
+        if (result.length > 0) {
+          res.status(200).json({ msg: "user already linked", err: "" });
+          resolve();
+        } else {
+          contract
+            .isOwner(ethers.utils.getAddress(ownerAddress))
+            .then((contractResult: any) => {
+              if (contractResult === true) {
+                const UserObj = Moralis.Object.extend("LinkedDaos");
+                const userSession = new UserObj();
+                userSession.set("daoAddress", daoAddress);
+                userSession.set("ownerAddress", ownerAddress);
+                userSession.set("chain", chain);
+                userSession.set("version", version);
+                userSession
+                  .save(null, { useMasterKey: true })
+                  .then((result: any) => {
+                    res.status(200).send("user saved");
+                    resolve();
+                  })
+                  .catch((err: any) => {
+                    console.log(err);
+                    res.status(403).send("error with setting user");
+                    resolve();
+                  });
+              } else {
+                res.status(403).send("user is not owner of contract");
+                resolve();
+              }
+            })
+            .catch((err: any) => {
+              console.log(err);
+              res
+                .status(403)
+                .send(
+                  "error with ethers.js call when checking user-dao ownership. try again"
+                );
+              resolve();
+            });
+        }
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.status(403).send(err);
+        resolve();
+      });
+  });
+};
+export default linkAccount;
