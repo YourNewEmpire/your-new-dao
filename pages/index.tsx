59463b7d501c80fdf98126fb3543eae371a5d8b5
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-24 12:00:41 +0000

    index - added authenticated status

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from "next";
+import { useMoralis } from "react-moralis";
+import AlertCard from "../components/Cards/AlertCard";
 const Home: NextPage = () => {
+  const { authenticate, isAuthenticated } = useMoralis();
   return (
     <div className="min-h-screen">
-      <div className="flex flex-col items-center justify-center text-center ">
-        <h1 className="text-6xl text-center font-semibold">Your New Dao</h1>
-        <p>A Marketplace of DAOs</p>
+      <div className="flex flex-col items-center justify-center space-y-4 lg:space-y-12 xl:space-y-20 text-center ">
+        <div>
+          <h1 className="text-6xl text-center font-semibold">Your New Dao</h1>
+          <p>A Marketplace of DAOs</p>
+        </div>
+        <AlertCard
+          warning
+          title="Wallet Authentication"
+          body={
+            <div>
+              STATUS:{" "}
+              {isAuthenticated ? (
+                <p className="text-th-accent-success-medium">CONNECTED</p>
+              ) : (
+                <p className="text-th-accent-failure-medium">NOT CONNECTED</p>
+              )}
+            </div>
+          }
+        />
       </div>
     </div>
   );
