d89f068d08ef3f716c5bb128f636bfb06e484ce6
YourNewEmpire <archiesmyth26@googlemail.com>
2022-01-25 19:35:14 +0000

    Added Modal component

---
+++ components/Modal/index.jsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+import Backdrop from "./Backdrop";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+const Modal = ({ handleClose, children }) => {
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="rounded-lg bg-th-foreground ring-2 ring-th-primary-medium p-6"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        {children}
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={handleClose}
+          className="
+            w-64 m-12 rounded-2xl font-semibold
+            bg-th-primary-medium 
+            "
+        >
+          CLOSE
+        </motion.button>
+      </motion.div>
+    </Backdrop>
+  );
+};
+
+export default Modal;
