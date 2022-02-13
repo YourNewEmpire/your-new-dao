4f07636cb3f08d2b08bca6bda17c9f86e5a7df6e
YourNewEmpire <archiesmyth26@googlemail.com>
2022-02-13 18:32:50 +0000

    testOwnership - added nft and owner tests

--- Hardhat/test/testOwnership.js
+++ Hardhat/test/testOwnership.js
@@ -1,25 +1,71 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-beforeEach(async function () {
-  // Get the ContractFactory and Signers here.
-  Dao = await ethers.getContractFactory("DAOV3");
-  [seller, buyer, thirdWheel] = await ethers.getSigners();
-  deployedDao = await Dao.deploy("InitialName");
-});
-
-describe("Deployment + DAO Purchase", function () {
-  it("Should purchase ownership", async function () {
-    const [seller, buyer, thirdWheel] = await ethers.getSigners();
-    const txOptions = { value: ethers.utils.parseEther("1.0") };
-    const buyerName = "Arch";
-    const daoName = buyerName + "Dao";
-    const tx = await deployedDao
+describe("DAO Contract", function () {
+  //? Set factory, provider, signers and names before deploying with second hh address (buyer).
+  beforeEach(async function () {
+    Dao = await ethers.getContractFactory("DAOV3");
+    provider = ethers.provider;
+    [seller, buyer, daoOwner2, daoOwner3] = await ethers.getSigners();
+    txOptions = { value: ethers.utils.parseEther("1.0") };
+    buyerName = "Arch";
+    daoName = buyerName + "Dao";
+    deployedDao = await Dao.deploy("InitialName");
+    daoPurchase = await deployedDao
       .connect(buyer)
       .buyOwnership(buyerName, daoName, txOptions);
-    await tx.wait();
-    expect(await deployedDao.isOwner(buyer.address)).to.equal(true);
-    expect(await deployedDao.name()).to.equal(daoName);
-    expect(await deployedDao.ownerName()).to.equal(buyerName);
+    await daoPurchase.wait();
+  });
+  describe("Deployment + DAO Purchase", function () {
+    it("Should purchase ownership and set names correctly", async function () {
+      expect(await deployedDao.isOwner(buyer.address)).to.equal(true);
+      expect(await deployedDao.name()).to.equal(daoName);
+      expect(await deployedDao.ownerName()).to.equal(buyerName);
+    });
+  });
+  describe("NFT Management", function () {
+    it("Should set new token IDs", async function () {
+      const newTokenIDs = ["COINS", "HELMET", "SWORD"];
+      const addNewTokens = await deployedDao
+        .connect(buyer)
+        .addNewTokenIDs(newTokenIDs);
+      await addNewTokens.wait();
+      expect(await deployedDao.tokenStringArray(0)).to.equal(newTokenIDs[0]);
+      expect(await deployedDao.tokenIdArray(1)).to.equal(1);
+      expect(await deployedDao.tokenIdMapping(newTokenIDs[0])).to.equal(0);
+      expect(await deployedDao.tokenIdMapping(newTokenIDs[1])).to.equal(1);
+    });
+
+    //todo - test base uri
+  });
+  describe("Owner Management", function () {
+    it("Should set new owners", async function () {
+      const addedOwners = await deployedDao
+        .connect(buyer)
+        .addOwners([daoOwner2.address, daoOwner3.address]);
+      await addedOwners.wait();
+      expect(await deployedDao.owners(1)).to.equal(daoOwner2.address);
+    });
+  });
+  //? Check balance is 1 before withdrawal, then withdraw and ensure balance is 0
+  describe("Seller Withdrawal and Contract + Account Balances ", function () {
+    it("Should have 1 ether balance", async function () {
+      expect(await deployedDao.contractBalance()).to.equal(txOptions.value);
+    });
+    it("Should withdraw balance", async function () {
+      const withdrawnEther = await deployedDao.connect(seller).withdraw();
+      await withdrawnEther.wait();
+      expect(await deployedDao.contractBalance()).to.equal(
+        txOptions.value - txOptions.value
+      );
+    });
+    it("Should change balances of seller and buyer", async function () {
+      const balance = async (addr) => {
+        const addrBalance = await provider.getBalance(addr);
+        return parseFloat(ethers.utils.formatEther(addrBalance));
+      };
+      expect(await balance(seller.address)).to.greaterThan(10000);
+      expect(await balance(buyer.address)).to.lessThan(10000);
+    });
   });
 });
