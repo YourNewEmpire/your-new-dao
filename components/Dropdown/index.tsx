dbdf1db101551cf3fd5528e54d3f0286ff07e256
YourNewEmpire <archiesmyth26@googlemail.com>
2022-02-24 13:34:19 +0000

    Dropdown - props typing hard-coded for UserDaos

--- components/Dropdown/index.tsx
+++ components/Dropdown/index.tsx
@@ -1,17 +1,20 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ArrowDownIcon } from '@heroicons/react/solid'
-
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ArrowDownIcon } from "@heroicons/react/solid";
+import { IUserDao } from "../../interfaces/userdao";
 
 // todo - could type these args
+//! Rewritten for object array instead of string array
 export default function Dropdown({ title, options, clickHandler }: any) {
-
-      return (
-            <div className='relative flex flex-col space-y-12
+  return (
+    <div
+      className="relative flex flex-col space-y-12
             justify-center items-center
-            p-6 webkit-antialiased'>
-                  <Menu>
-                        <Menu.Button className="
+            p-6 webkit-antialiased"
+    >
+      <Menu>
+        <Menu.Button
+          className="
                         flex flex-row space-x-2
                         justify-center items-center
                         p-2 xl:p-2 text-xl
@@ -23,47 +26,48 @@ export default function Dropdown({ title, options, clickHandler }: any) {
                         w-32
                         md:w-52
                         xl:w-80
-                        ">
-                              <p className=' text-sm md:text-lg xl:text-2xl truncate'>
-                                    {title? title: "select"}
-                              </p>
-                              <ArrowDownIcon width={50} height={50} />
-                        </Menu.Button>
+                        "
+        >
+          <p className=" text-sm md:text-lg xl:text-2xl truncate">
+            {title ? title : "select"}
+          </p>
+          <ArrowDownIcon width={30} height={30} />
+        </Menu.Button>
 
-                        <Transition
-                              as={Fragment}
-                              enter="transition ease-out duration-100"
-                              enterFrom="transform opacity-0 scale-95"
-                              enterTo="transform opacity-100 scale-100"
-                              leave="transition ease-in duration-75"
-                              leaveFrom="transform opacity-100 scale-100"
-                              leaveTo="transform opacity-0 scale-95"
-                        >
-
-                              <Menu.Items className="
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items
+            className="
                               p-2 rounded-md text-lg
                               absolute flex flex-col top-10 
-                              bg-opacity-50 bg-cyan-600
+                              bg-opacity-50 bg-th-primary-dark
                               text-center
                               z-50
-                              ">
-                                    {options.map((item: string, index: number) =>
-                                          <Menu.Item key={index}>
-                                                <button
-                                                      onClick={() => clickHandler(item)}
-                                                      className='
+                              "
+          >
+            {options.map((item: IUserDao, index: number) => (
+              <Menu.Item key={index}>
+                <button
+                  onClick={() => clickHandler(item)}
+                  className="
                                                       rounded-md px-2
                                                       transition duration-200 ease-in-out 
-                                                      hover:bg-indigo-800 hover:cursor-pointer'
-                                                >
-                                                      {item}
-                                                </button>
-                                          </Menu.Item>
-                                    )}
-
-                              </Menu.Items>
-                        </Transition>
-                  </Menu>
-            </div>
-      )
+                                                      hover:bg-indigo-800 hover:cursor-pointer"
+                >
+                  {item.address}
+                </button>
+              </Menu.Item>
+            ))}
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
 }
